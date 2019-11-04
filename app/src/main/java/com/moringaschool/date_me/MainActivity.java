b78e52363aa1cef5fb2383f3a8a80a48222ab0ae
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-11-04 16:29:11 +0200

    added app/

---
+++ app/src/main/java/com/moringaschool/date_me/MainActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.date_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+ import com.moringaschool.date_me.R;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
