87c755252ba6c743e55ed2bf9b32c6a1aa7a8d52
GraceUmutesi <tesigrace98@gmail.com>
2019-11-05 22:21:14 +0200

    styling the front page

---
+++ app/src/main/java/com/moringaschool/date_me/FrontPage.java
@@ -0,0 +1,16 @@
+package com.moringaschool.date_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class FrontPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+
+
+    }
+}
