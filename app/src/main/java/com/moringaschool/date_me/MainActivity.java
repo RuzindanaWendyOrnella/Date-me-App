9c1b59dd67f4634be5dbbe6e6f5058eca311f3f0
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-11-07 04:27:42 +0200

    created pages for chats and users

--- app/src/main/java/com/moringaschool/date_me/MainActivity.java
+++ app/src/main/java/com/moringaschool/date_me/MainActivity.java
@@ -1,8 +1,13 @@
 package com.moringaschool.date_me;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -14,10 +19,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +33,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.moringaschool.date_me.R;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,8 +67,15 @@ public class MainActivity extends AppCompatActivity {
                 } else {                }
             }
         };
-
-    }    @Override
+        TabLayout tableLayout=(findViewById(R.id.tab_layout));
+        ViewPager viewPager=(findViewById(R.id.view_pager));
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        viewPager.setAdapter(viewPagerAdapter);
+        tableLayout.setupWithViewPager(viewPager);
+    }
+    @Override
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
@@ -69,7 +85,10 @@ public class MainActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
-    }    @Override
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
@@ -88,6 +107,36 @@ public class MainActivity extends AppCompatActivity {
         logIn.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(logIn);
         finish();
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+     private ArrayList<Fragment> fragments;
+     private ArrayList<String> titles;
+      ViewPagerAdapter(FragmentManager fm){
+          super(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+          this.fragments=new ArrayList<>();
+          this.titles=new ArrayList<>();
+      }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+        public void addFragment(Fragment fragment,String tittle){
+          fragments.add(fragment);
+          titles.add(tittle);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
 
